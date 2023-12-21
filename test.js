83250926e12de2eb2d5359c09b5cee40983d64d5
dyanix <dyanixdhawale@gmail.com>
2023-12-22 00:02:25 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,27 @@
+console.log('Hello')
+
+
+const accountId = 1232
+
+let email = "a@gmail.com"
+
+var password = "12223e4"
+
+let accountState
+
+city = "pune"
+// accountId=2322
+
+email ="hell@gmail.com"
+
+password = 'q23e323'
+
+
+city="bengluru"
+/*
+prefer not to use var b/c issue in block scope and functional scope
+
+
+*/
+
+console.table([accountId,email,password,city,accountState])
---
+++ test2.js
@@ -0,0 +1,33 @@
+"use strict" 
+//treat all js code as newer version
+
+
+
+// alert(2+2)   
+// using  node js not browser
+
+
+let score  = "30qw"
+
+// console.log(typeof(score));  //string
+
+let convertinNumber = Number(score)
+
+// console.log(typeof convertinNumber);  //number
+// console.log(convertinNumber);   // NaN
+
+
+let loggedIn = "1"
+
+
+let convertinBoolean = Boolean(loggedIn)
+
+console.log(typeof loggedIn);  //number
+
+console.log( convertinBoolean);  // true
+// 1 => true
+// 0 => false
+// "" => false
+// "has" =>  true
+
+
