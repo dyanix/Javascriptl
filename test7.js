d3cb47aacb00736b952535b76a766ddd10aeb780
dyanix <dyanixdhawale@gmail.com>
2024-01-01 14:49:26 +0530

    js

---
+++ test7.js
@@ -0,0 +1,20 @@
+//Json api 
+
+
+// some values are coming from backend earlier they are in  xml structure which were very complex.
+// now in json
+
+// {
+//     "courseNam"e:"Js",
+//     "price":99,
+//     "courseInstructor":"Histesh Sir"
+// }
+
+
+// api may also in array format
+
+// [
+//     {},
+//     {}
+// ]
+
