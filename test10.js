fa1efbae048e392ad50594aaf826d39140c6a323
dyanix <dyanixdhawale@gmail.com>
2024-01-04 01:08:43 +0530

    js

---
+++ test10.js
@@ -0,0 +1,68 @@
+// this is only used in thr object 
+// not in function
+
+
+
+const user = {
+    username:"Dyanesh",
+    Price:232,
+
+    welcomeMessage:function(){
+        console.log(`${this.username},welcome`);
+        console.log(this);
+    }
+
+}
+
+
+// user.welcomeMessage()
+
+// user.username = "Dyanix"
+
+// user.welcomeMessage()
+
+// console.log(this);
+
+
+//this not used in fuction 
+//it use in the property of the object
+// function chai(){
+//     let name="dyanesh"
+//     console.log(this.name);
+// }
+
+// chai()
+
+
+// const chai = function(){
+//     let name="dyanesh"
+//     console.log(this.name);
+// }
+
+
+
+// const chai = ()=>{
+//     let name="dyanesh"
+//     console.log(this.name);
+// }
+
+
+// chai()
+
+//simple arrow function
+
+// const addtwo = (num1,num2) => {
+//     return num1+num2
+
+// }
+
+
+
+//implicit arrow function
+
+const addtwo = (num1,num2) =>  ( num1+num2)
+
+
+
+
+console.log(addtwo(3,4));
