83aa358b191eb5dd6c52e0068458f94e690c0852
dyanix <dyanixdhawale@gmail.com>
2024-01-06 00:51:37 +0530

    js

---
+++ test13.js
@@ -0,0 +1,40 @@
+
+
+
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 7){
+//         console.log("Thala for reason");
+//     }
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outter loop ${i}`);
+//    for (let j = 0; j <10; j++) {
+//    console.log(`Inner loop ${j}`);
+    
+//    }
+    
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Table of ${i}`);
+//    for (let j = 1; j <=10; j++) {
+//    console.log(i + "*" + j + "=" + i*j);
+    
+//    }
+    
+// }
+
+
+// let array = ["hello" ,"Dyanesh" ,"hi"]
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element);
+    
+// }
+
