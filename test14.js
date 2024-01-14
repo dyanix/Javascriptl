a77d7793f2ca76ecba23a1f2231145b8d5a55cc9
dyanix <dyanixdhawale@gmail.com>
2024-01-15 00:32:07 +0530

    js

---
+++ test14.js
@@ -0,0 +1,60 @@
+// static with pure html 
+//javascript is high level language to make website interactive
+// standard implementation is callled Ecmascript default language of webbrowser 
+// on runtime javascript can be run nodejs dino
+//javascript is scripting language we can run code on console on dev tools 
+// interpreted line by line however it is not accurate term to use 
+//under hood of browser there is v8 engine which make javascript run extemely fast
+
+//   CODE ------ V8 JIT(JUST IN TIME COMPILATION)
+
+//Dynamically typed language 
+
+
+// function add(a,b){
+//     return a+b;
+// }
+
+//HIGHERORDER FUNCTION
+
+// function higherorder(func){
+//     func();
+//     return function(){
+
+//     }
+
+// }
+
+// function name1() {
+//     console.log(this); 
+
+//global object/window
+    
+// }
+// name1()
+
+
+// const num = 23;
+// const obj = new Object();
+
+// somefun(num,obj)
+
+// num --> primitive is pass by value
+//obj ---> passed by reference
+
+
+
+
+// const list = [1,1,2,3,4]
+
+// const unique = new Set(list)
+
+// console.log(unique);
+
+
+
+// setTimeout(()=>{
+//     console.log("5 seconds HI");
+
+// },5000)
+
