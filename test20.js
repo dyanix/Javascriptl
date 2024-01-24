b69365cfde99b7782521840edceb0c71edb11f32
dyanix <dyanixdhawale@gmail.com>
2024-01-24 18:26:05 +0530

    js

--- test20.js
+++ test20.js
@@ -18,7 +18,7 @@ async function data(){
     let raw = await fetch(`https://randomuser.me/api/`)
     let ans = await raw.json()
     console.log(ans);
-    
+   
 
 
 }
@@ -29,3 +29,17 @@ data()
 //concurrency --> running sync and async code simultaneously
 //parallelism --> processora and cores
 //throtteling --> control code for number of execution
+
+
+//reverse string
+
+const named = "hello"
+const arr = []
+
+for (let i = named.length ; i >=0; i--) {
+    const element = named[i];
+    arr.push(element)
+    
+}
+
+console.log(arr.join(""));
