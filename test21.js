10702c414200cfd2921f63d35188255ceb520fc4
dyanix <dyanixdhawale@gmail.com>
2024-02-19 23:31:18 +0530

    js

---
+++ test21.js
@@ -0,0 +1,14 @@
+
+// hoisting
+
+// Global execution context 
+// Memory Phase were all the variables loaded then we have Code Phase 
+
+
+// console.log("Age is",age);
+// var age = 20
+// console.log("Age is",age);
+
+// age=100
+console.log("Age is",age);
+let age=30
